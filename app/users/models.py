273ec0706c209c2d1380b7dbdf2fc690a0cc5d76
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-04 03:26:41 -0600

    Add initials to user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 from .managers import UserManager
 from common.models import BaseModel
 
 
 class User(AbstractUser, BaseModel):
+    initials = models.CharField(max_length=3, null=True, blank=True, unique=True)
     is_approved = models.BooleanField(
         "Approval status",
         default=False,
