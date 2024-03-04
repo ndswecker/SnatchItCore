c03b9016a47142078f8d82729bb7cd9b6ab7517a
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-04 03:27:40 -0600

    Refactor fieldsets, list, and filter

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 class UserAdmin(AuthUserAdmin):
-    pass
+    list_display = ("username", "email", "first_name", "last_name", "initials", "is_approved", "is_staff", "is_superuser")
+    list_filter = ("is_approved", "is_staff", "is_superuser", "is_active", "groups")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "initials")}),
+        (_("Permissions"), {"fields": ("is_active", "is_approved", "is_staff", "is_superuser", "groups", "user_permissions")}),  # noqa
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
 
 admin.site.register(User, UserAdmin)
