629aae69d1cc4e7c846d125d401820935acd6e73
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-12 22:47:52 -0500

    Split validate to decrease algorithmic complexity

--- app/common/validators.py
+++ app/common/validators.py
@@ -9,14 +9,19 @@ class FormValidator:
         self.validators = []
 
     def validate(self, override_validation: bool):
-        for validator in self.validators:
-            if override_validation:
-                try:
-                    validator(self.cleaned_data)
-                except ValidationError as e:
-                    self.validation_errors.append(e.messages[0])
-            else:
-                validator(self.cleaned_data)
         if override_validation:
-            self.discrepancy_string = "\n".join(err_str for err_str in self.validation_errors).strip("\n")
+            return self._catch_validation_errors()
+        else:
+            return self._raise_validation_errors()
 
+    def _raise_validation_errors(self):
+        for validator in self.validators:
+            validator(self.cleaned_data)
+
+    def _catch_validation_errors(self):
+        for validator in self.validators:
+            try:
+                validator(self.cleaned_data)
+            except ValidationError as e:
+                self.validation_errors.append(e.messages[0])
+        self.discrepancy_string = "\n".join(err_str for err_str in self.validation_errors).strip("\n")
