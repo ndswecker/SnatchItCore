53d97e1f7b045f6844eb08ea1810b79ca87382ba
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-03-12 19:19:10 -0700

    Implement skull to age validator
    Error exists in is validated

--- app/maps/validators.py
+++ app/maps/validators.py
@@ -1,6 +1,7 @@
 from django.core.validators import ValidationError
 
 from common.validators import FormValidator
+from maps.maps_reference_data import SPECIES
 
 
 def validate_juv_aging(form_data: dict):
@@ -12,10 +13,33 @@ def validate_juv_aging(form_data: dict):
             },
         )
 
+def validate_skull_to_age(form_data: dict):
+    if (form_data.get("how_aged_1") == "S" or form_data.get("how_aged_2") == "S") and not form_data.get("skull"):
+        raise ValidationError(
+            {
+                "age_annual": f"A skull score must be provided if how aged is by skull.",
+            },
+        )
+    
+    if form_data.get("skull") and (form_data.get("skull") < 5 and form_data.get("age_annual") not in [2, 4]):
+        raise ValidationError(
+            {
+                "age_annual": f"A skull score of {form_data.get('skull')} is only valid for HY or Local birds.",
+            },
+        )
+    
+    if form_data.get("skull") in [5, 6] and form_data.get("age_annual") in [2, 4]:
+        raise ValidationError(
+            {
+                "age_annual": f"A skull score of {form_data.get('skull')} is not valid for HY or Local birds.",
+            },
+        )
+
 
 class CaptureRecordFormValidator(FormValidator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.validators = [
             validate_juv_aging,
+            validate_skull_to_age,
         ]
