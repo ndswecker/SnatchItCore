2ae85c9e6b38c0f4b0ce1e0363755339213abc33
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-16 14:36:16 -0500

    Use FormHelper

--- app/maps/forms.py
+++ app/maps/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 from django_select2 import forms as s2forms
 
@@ -8,6 +10,12 @@ from maps.validators import CaptureRecordFormValidator
 
 
 class CaptureRecordForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit('submit', 'Submit', css_class="btn btn-lg btn-primary w-100"))
+
     species_number = forms.ChoiceField(
         choices=SPECIES_CHOICES,
         widget=s2forms.Select2Widget(attrs={"class": "form-control"}),
