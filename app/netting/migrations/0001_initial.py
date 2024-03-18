abdf481ee5c226edbf2e7e1c642d95c59e5eb122
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-17 22:17:48 -0500

    Create migrations

---
+++ app/netting/migrations/0001_initial.py
@@ -0,0 +1,59 @@
+import django.db.models.deletion
+import uuid
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="NettingRecord",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "uuid",
+                    models.UUIDField(
+                        db_index=True, default=uuid.uuid4, editable=False, unique=True
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("period", models.IntegerField()),
+                ("date", models.DateField()),
+                ("numbers", models.CharField(max_length=255)),
+                ("open_time", models.TimeField()),
+                ("close_time", models.TimeField()),
+                ("hours", models.DecimalField(decimal_places=2, max_digits=5)),
+                (
+                    "period_net_hours",
+                    models.DecimalField(decimal_places=2, max_digits=5),
+                ),
+                ("note_number", models.IntegerField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+            options={
+                "abstract": False,
+            },
+        ),
+    ]
