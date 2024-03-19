04f55e4230c4303cb84600c14863cb83b003fd64
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-03-18 17:03:25 -0700

    Eliminate white spaces and make quotes consistent

--- app/maps/views.py
+++ app/maps/views.py
@@ -37,10 +37,10 @@ class DetailCaptureRecordView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        capture_record = context['capture_record']
+        capture_record = context["capture_record"]
         species_number = capture_record.species_number
         species_info = REFERENCE_DATA.SPECIES[species_number]
-        context['species_name'] = species_info['common_name']
+        context["species_name"] = species_info["common_name"]
         return context
 
 
