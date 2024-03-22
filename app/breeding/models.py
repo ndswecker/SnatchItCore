afad5815605b49a44264de089febf8ab1bc36e08
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-22 00:35:02 -0500

    Create station, status, and report models

--- app/breeding/models.py
+++ app/breeding/models.py
@@ -4,20 +4,25 @@ from common.models import BaseModel
 from users.models import User
 
 
-class BreedingRecord(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    hawo = models.CharField("HAWO", max_length=100)
-    acfl = models.CharField("ACFL", max_length=100)
-    ytvi = models.CharField("YTVI", max_length=100)
-    revi = models.CharField("REVI", max_length=100)
-    tres = models.CharField("TRES", max_length=100)
-    cach = models.CharField("CACH", max_length=100)
-    woth = models.CharField("WOTH", max_length=100)
-    gcth = models.CharField("GCTH", max_length=100)
-    amro = models.CharField("AMRO", max_length=100)
-    grca = models.CharField("GRCA", max_length=100)
-    nopa = models.CharField("NOPA", max_length=100)
-    ywar = models.CharField("YWAR", max_length=100)
-    amre = models.CharField("AMRE", max_length=100)
-    initials = models.CharField(max_length=100)
+class Station(BaseModel):
+    name = models.CharField(max_length=256)
+
+
+class Report(BaseModel):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    year = models.IntegerField()
+
+
+class Status(BaseModel):
+    #  TODO: associate status with specific report
+    # report = models.ForeignKey(Report, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    species = models.CharField(max_length=4)  # TODO: use species number
+    period = models.IntegerField()
+    status = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = "Statuses"
+
+    def __str__(self):
+        return f"{self.species} - {self.period} - {self.status}"
