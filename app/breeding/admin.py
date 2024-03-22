6ad0f20909ef9f890c2fec74984796475ac3db77
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-22 00:35:25 -0500

    Create admin and serializer placeholders

--- app/breeding/admin.py
+++ app/breeding/admin.py
@@ -1,10 +1,44 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils import timezone
 
-from breeding.models import BreedingRecord
+from breeding.models import Report
+from breeding.models import Station
+from breeding.models import Status
+from breeding.serializers import StatusSerializer
 
 
-class BreedingRecordAdmin(admin.ModelAdmin):
+class StationAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(BreedingRecord, BreedingRecordAdmin)
+admin.site.register(Station, StationAdmin)
+
+
+class ReportAdmin(admin.ModelAdmin):
+    pass
+
+    # @admin.action(description="Export report CSV")
+    # def export_csv(self, request, queryset):
+    #     response = HttpResponse(content_type="text/csv")
+    #     response["Content-Disposition"] = (
+    #         f"attachment; filename={timezone.now().strftime('%Y-%m-%d %H:%M:%S')}_USGS.csv"  # noqa
+    #     )
+    #     writer = csv.writer(response)
+    #     writer.writerow(ReportSerializer(queryset.first()).serialize().keys())
+    #     for obj in queryset:
+    #         writer.writerow(StatusSerializer(obj).serialize().values())
+    #     return response
+
+
+admin.site.register(Report, ReportAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Status, StatusAdmin)
---
+++ app/breeding/serializers.py
@@ -0,0 +1,12 @@
+from breeding.models import Report
+from breeding.models import Status
+
+
+class ReportSerializer:
+    def __init__(self):
+        pass
+
+
+class StatusSerializer:
+    def __init__(self):
+        pass
