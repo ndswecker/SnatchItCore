60975cdfb435f343ed8de728ad8d5626f7f4d556
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-03-25 10:29:25 -0700

    Calculate hold time in the view

--- app/maps/views.py
+++ app/maps/views.py
@@ -7,6 +7,7 @@ from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import TemplateView
+from django.utils import timezone
 
 import maps.maps_reference_data as REFERENCE_DATA
 from .forms import CaptureRecordForm
@@ -24,13 +25,27 @@ class CreateCaptureRecordView(LoginRequiredMixin, ApprovalRequiredMixin, CreateV
     template_name = "maps/enter_bird.html"
     form_class = CaptureRecordForm
 
+    def get(self, request, *args, **kwargs):
+        # Set start time in session when the form is first loaded
+        request.session['form_start_time'] = timezone.now().isoformat()
+        return super().get(request, *args, **kwargs)
+
     def form_valid(self, form):
+        # Retrieve start time from session and convert it back to a datetime object
+        start_time_str = self.request.session.pop('form_start_time', None)
+        start_time = timezone.datetime.fromisoformat(start_time_str)
+
+        submission_time = timezone.now()
+        
+        # Calculate time held and set it on the instance before saving
+        time_held = submission_time - start_time
+        form.instance.hold_time = time_held.total_seconds() / 60
+
         form.instance.user = self.request.user
         form.instance.bander_initials = self.request.user.initials
         self.object = form.save()
         messages.success(self.request, "Capture record created successfully.")
         return redirect(reverse_lazy("maps:detail_capture_record", kwargs={"pk": self.object.pk}))
-
 
 class DetailCaptureRecordView(LoginRequiredMixin, DetailView):
     template_name = "maps/detail.html"
