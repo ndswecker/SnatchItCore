1f6370f16f36c9e7a66793dfb9885d568af7101f
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-25 20:46:02 -0500

    Add lan option

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,3 +1,11 @@
+param(
+    [switch]$lan
+)
+
 .venv\Scripts\activate
 
-python app\manage.py runserver localhost:80
+if ($lan) {
+    python app\manage.py runserver 0.0.0.0:80
+} else {
+    python app\manage.py runserver localhost:80
+}
