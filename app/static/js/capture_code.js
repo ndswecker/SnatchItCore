cefeb47df998266ba4b8c266c6d14917ceb1dcea
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-03-25 20:27:59 -0700

    Include the full automation of unbanded and recapture form filling

--- app/static/js/capture_code.js
+++ app/static/js/capture_code.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
     $('#id_capture_code').on('change', function() {
-        console.log('Capture code changed');
         var captureCode = $(this).val();
-        var bandSizeSelect = $('#id_band_size'); // Assuming the ID for the band size select field is 'id_band_size'
+        var bandSizeSelect = $('#id_band_size');
+        var bandNumberInput = $('#id_band_number');
+        var statusSelect = $('#id_status'); // Assuming the ID for the status select field is 'id_status'
 
         if (captureCode === 'R') {
-            // Set the band size select field to "R" and trigger change event for Select2 to update
-            bandSizeSelect.val('R').trigger('change');
-            
-            // Optionally, if you want to make the band size field readonly/disabled when "R" is selected
-            // You might need to also handle re-enabling it if a different capture code is selected
-            bandSizeSelect.prop('disabled', true);
+            // For recaptured birds, set band size to "R", disable the field, and log the change
+            bandSizeSelect.val('R').trigger('change').prop('disabled', true);
+            console.log('Capture code set to Recaptured. Band size locked to "R".');
+        } else if (captureCode === 'U') {
+            // For unbanded birds, set band size to "U", clear band number, but do not disable the band size field
+            // Additionally, set the status select to "0" for unbanded birds
+            bandSizeSelect.val('U').trigger('change');
+            bandNumberInput.val('');
+            statusSelect.val('0').trigger('change'); // Set status to "0"
+            console.log('Capture code set to Unbanded. Band size set to "U", band number cleared, and status set to "0".');
         } else {
-            // Re-enable the band size field if another capture code is selected
+            // For any other capture code, ensure band size, band number, and status fields are enabled
             bandSizeSelect.prop('disabled', false);
+            bandNumberInput.prop('disabled', false);
+            statusSelect.prop('disabled', false); // Ensure the status field is enabled for other capture codes
+            console.log('Capture code changed. Band size, band number, and status fields are enabled.');
         }
     });
 });
-
