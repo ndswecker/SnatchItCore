e75a12411b8dabf2201edb46648b067a1e4f0f46
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-04-07 20:32:02 -0700

    Single js for all info modals

--- app/static/js/help_images.js
+++ app/static/js/help_images.js
@@ -1,53 +1,22 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Cloacal Protuberance Modal
-    // Get the button and the modal
-    var cpButton = document.querySelector('label[for="id_cloacal_protuberance"]');
-    var cpModal = document.getElementById('cpModal');
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to initialize a modal
+    function initModal(buttonSelector, modalId) {
+        const button = document.querySelector(buttonSelector);
+        const modal = document.getElementById(modalId);
 
-    // Check if the button and the modal exist
-    if (cpButton && cpModal) {
-        // Create a new Bootstrap Modal instance
-        var cpModalInstance = new bootstrap.Modal(cpModal);
-
-        // Add a click event listener to the button
-        cpButton.addEventListener('click', (event) => {
-            console.log('cpButton clicked');
-            // Prevent the button's default action
-            event.preventDefault();
-
-            // Show the modal
-            cpModalInstance.show();
-        });
+        if (button && modal) {
+            const modalInstance = new bootstrap.Modal(modal);
+            button.addEventListener('click', (event) => {
+                console.log(`${modalId} button clicked`);
+                event.preventDefault();
+                modalInstance.show();
+            });
+        }
     }
 
-    // Brood Patch Modal
-    // Get the button and the modal
-    var bpButton = document.querySelector('label[for="id_brood_patch"]');
-    var bpModal = document.getElementById('bpModal');
-
-    // Check if the button and the modal exist
-    if (bpButton && bpModal) {
-        // Create a new Bootstrap Modal instance
-        var bpModalInstance = new bootstrap.Modal(bpModal);
-
-        // Add a click event listener to the button
-        bpButton.addEventListener('click', (event) => {
-            console.log('bpButton clicked');
-            // Prevent the button's default action
-            event.preventDefault();
-
-            // Show the modal
-            bpModalInstance.show();
-        });
-    }
-
-      // Select the label using the 'for' attribute
-    const cpLabel = document.querySelector('label[for="id_cloacal_protuberance"]');
-
-    // Make sure the element exists before adding the event listener
-    if (cpLabel) {
-        cpLabel.addEventListener('click', function() {
-        console.log('CP label was clicked!');
-        });
-  }
+    // Initialize each modal
+    initModal('label[for="id_cloacal_protuberance"]', 'cpModal');
+    initModal('label[for="id_brood_patch"]', 'bpModal');
+    initModal('label[for="id_ff_wear"]', 'ffWearModal');
+    // Add more as needed
 });
