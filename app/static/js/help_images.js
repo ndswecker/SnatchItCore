5c1694cef0275e72a609cd266198dca731951911
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-04-07 18:34:36 -0700

    Add image reference modals to morphometrics

--- app/static/js/help_images.js
+++ app/static/js/help_images.js
@@ -1,92 +1,53 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the div where you want to insert the modal
-    var cp_div = document.getElementById('div_id_cloacal_protuberance');
+    // Cloacal Protuberance Modal
+    // Get the button and the modal
+    var cpButton = document.querySelector('label[for="id_cloacal_protuberance"]');
+    var cpModal = document.getElementById('cpModal');
 
-    // Create the modal button
-    var cpModalButton = document.createElement('button');
-    cpModalButton.type = 'button';
-    cpModalButton.id = 'view-cp-info-modal';
-    cpModalButton.className = 'btn btn-lg btn-danger w-100';
-    cpModalButton.dataset.bsToggle = 'modal';
-    cpModalButton.dataset.bsTarget = '#cpModal';
-    cpModalButton.textContent = 'CP Scores';
+    // Check if the button and the modal exist
+    if (cpButton && cpModal) {
+        // Create a new Bootstrap Modal instance
+        var cpModalInstance = new bootstrap.Modal(cpModal);
 
-    // Insert the modal button into the div
-    cp_div.appendChild(cpModalButton);
+        // Add a click event listener to the button
+        cpButton.addEventListener('click', (event) => {
+            console.log('cpButton clicked');
+            // Prevent the button's default action
+            event.preventDefault();
 
-    // Create the modal
-    var cpModal = document.createElement('div');
-    cpModal.className = 'modal fade';
-    cpModal.id = 'cpModal';
-    cpModal.tabIndex = '-1';
-    cpModal.ariaLabelledby = 'cpModalLabel';
-    cpModal.ariaHidden = 'true';
-    cpModal.innerHTML = `
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="cpModalLabel">CP Scores</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <img src="/static/img/cloacal_protuberance.png" alt="Cloacal Protuberance" class="img-fluid">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    `;
+            // Show the modal
+            cpModalInstance.show();
+        });
+    }
 
-    // Insert the modal into the body
-    document.body.appendChild(cpModal);
+    // Brood Patch Modal
+    // Get the button and the modal
+    var bpButton = document.querySelector('label[for="id_brood_patch"]');
+    var bpModal = document.getElementById('bpModal');
 
-    // Initialize the modal
-    var cpModalInstance = new bootstrap.Modal(cpModal);
+    // Check if the button and the modal exist
+    if (bpButton && bpModal) {
+        // Create a new Bootstrap Modal instance
+        var bpModalInstance = new bootstrap.Modal(bpModal);
 
-    // Get the div where you want to insert the modal
-    var bp_div = document.getElementById('div_id_brood_patch');
+        // Add a click event listener to the button
+        bpButton.addEventListener('click', (event) => {
+            console.log('bpButton clicked');
+            // Prevent the button's default action
+            event.preventDefault();
 
-    // Create the modal button
-    var bpModal = document.createElement('button');
-    bpModal.type = 'button';
-    bpModal.id = 'view-bp-info-modal';
-    bpModal.className = 'btn btn-lg btn-danger w-100';
-    bpModal.dataset.bsToggle = 'modal';
-    bpModal.dataset.bsTarget = '#bpModal';
-    bpModal.textContent = 'BP Scores';
+            // Show the modal
+            bpModalInstance.show();
+        });
+    }
 
-    // Insert the modal button into the div
-    bp_div.appendChild(bpModal);
+      // Select the label using the 'for' attribute
+    const cpLabel = document.querySelector('label[for="id_cloacal_protuberance"]');
 
-    // Create the modal
-    var modal = document.createElement('div');
-    modal.className = 'modal fade';
-    modal.id = 'bpModal';
-    modal.tabIndex = '-1';
-    modal.ariaLabelledby = 'bpModalLabel';
-    modal.ariaHidden = 'true';
-    modal.innerHTML = `
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="bpModalLabel">BP Scores</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <img src="/static/img/brood_patch.png" alt="Brood Patch" class="img-fluid">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    `;
-
-    // Insert the modal into the body
-    document.body.appendChild(modal);
-
-    // Initialize the modal
-    var bpModal = new bootstrap.Modal(modal);
+    // Make sure the element exists before adding the event listener
+    if (cpLabel) {
+        cpLabel.addEventListener('click', function() {
+        console.log('CP label was clicked!');
+        });
+  }
 });
-
