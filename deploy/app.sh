defb95874565e6755e287237195e2466736e41f0
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-04 16:10:22 -0700

    Load data

--- deploy/app.sh
+++ deploy/app.sh
@@ -10,3 +10,13 @@ SECRET_KEY=$(python3 -c "import secrets;print(secrets.token_urlsafe(64))")
 sed -i "s/<SECRET_KEY>/$SECRET_KEY/g" /srv/web/.env
 /srv/web/venv/bin/python3 /srv/web/app/manage.py collectstatic
 /srv/web/venv/bin/python3 /srv/web/app/manage.py migrate
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_bands /srv/web/data/Bands.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_ageannuals /srv/web/data/AgeAnnuals.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_agewrps /srv/web/data/AgeWRPs.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_groupwrps /srv/web/data/GroupWRPs.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_species /srv/web/data/TaxonBBL.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_band_allocations /srv/web/data/BandAllocations.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py setup_taxon_band_relationships
+/srv/web/venv/bin/python3 /srv/web/app/manage.py import_morphometrics /srv/web/data/Morphometrics.csv
+/srv/web/venv/bin/python3 /srv/web/app/manage.py setup_taxon_relationships
+
