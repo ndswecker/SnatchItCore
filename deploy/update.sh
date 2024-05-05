bd003ebd40f1a3556c18d2c29def6d6a80c9a626
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-04 17:54:08 -0700

    Ignore ufw

--- deploy/update.sh
+++ deploy/update.sh
@@ -17,11 +17,11 @@ git pull
 
 systemctl stop gunicorn.socket
 systemctl stop gunicorn.service
-systemctl stop ufw
+# systemctl stop ufw
 systemctl stop nginx
 
 systemctl daemon-reload
 systemctl start gunicorn.socket
 systemctl enable gunicorn.socket
-systemctl restart ufw
+# systemctl restart ufw
 systemctl restart nginx
