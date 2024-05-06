e3f280643cf717f3aa520a08558238a5fe526ff4
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-06 13:24:32 -0700

    Use date range to filter capture records

--- app/charts/views.py
+++ app/charts/views.py
@@ -2,69 +2,46 @@ import plotly.express as px
 from django.db.models import Count
 from django.views.generic import TemplateView
 
+from django.utils.html import format_html
+from django.utils.dateparse import parse_date
+
 from maps.maps_reference_data import SPECIES
 from maps.models import CaptureRecord
-
+from .forms import DateForm
 
 class BirdsView(TemplateView):
     template_name = "charts/birds.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        date = self.request.GET.get("date", None)
-        context["chart_species_capture_count"] = self.get_chart_species_capture_count(date)
-        context["chart_sex_capture_count"] = self.get_chart_sex_capture_count(date)
-        context["chart_age_capture_count"] = self.get_chart_age_capture_count(date)
+
+        date_form = DateForm(self.request.GET)
+        context["date_form"] = date_form
+        date_range = None
+
+        start_date_str = self.request.GET.get("start")
+        end_date_str = self.request.GET.get("end")
+
+        if start_date_str and end_date_str:
+            start_date = parse_date(start_date_str)
+            end_date = parse_date(end_date_str)
+            date_range = (start_date, end_date)
+
+        context["chart_species_capture_count"] = self.get_chart_species_capture_count(date_range)
+        context["chart_sex_capture_count"] = self.get_chart_sex_capture_count(date_range)
+        context["chart_age_capture_count"] = self.get_chart_age_capture_count(date_range)
+
         return context
 
-    # def get_chart_species_capture_count(self, date=None):
-    #     if date:
-    #         data = CaptureRecord.objects.filter(capture_time__date=date).values("species_number").annotate(capture_count=Count("species_number"))
-    #     else:
-    #         data = CaptureRecord.objects.values("species_number").annotate(capture_count=Count("species_number"))
 
-    #     x_values = [SPECIES[d["species_number"]]["alpha_code"] for d in data]
-    #     y_values = [d["capture_count"] for d in data]
-
-    #     # Get the number of x_values that will be displayed in the chart
-    #     x_values_count = len(x_values)
-
-    #     # Get the capture count of the 5th most captured species
-    #     bottom_cutoff = sorted(y_values, reverse=True)[4]
-    #     print(bottom_cutoff)
-
-    #     if not (x_values and y_values):
-    #         return ""
-        
-    #     colors = ["red" if count >= bottom_cutoff else "blue" for count in y_values]
-
-    #     fig = px.bar(
-    #         x=x_values,
-    #         y=y_values,
-    #         title="Species Capture Count",
-    #         labels={
-    #             "x": "Species",
-    #             "y": "Capture Count",
-    #         },
-    #         text_auto=True,
-    #         color=colors,
-    #     )
-    #     fig.update_traces(textfont_size=14, textangle=0, textposition="inside")
-    #     fig.update_layout(
-    #         title={
-    #             "font_size": 22,
-    #             "xanchor": "center",
-    #             "x": 0.5,
-    #         },
-    #         showlegend=False,
-    #     )
-    #     return fig.to_html()
-
-    def get_chart_species_capture_count(self, date=None):
-        if date:
-            data = CaptureRecord.objects.filter(capture_time__date=date).values("species_number").annotate(capture_count=Count("species_number"))
+    def get_chart_species_capture_count(self, date_range=None):
+        if date_range:
+            start_date, end_date = date_range
+            data = CaptureRecord.objects.filter(capture_time__date__range=[start_date, end_date]).values("species_number").annotate(capture_count=Count("species_number"))
+            title = f"Species Captured from {start_date} to {end_date}"
         else:
             data = CaptureRecord.objects.values("species_number").annotate(capture_count=Count("species_number"))
+            title = "Species Capture Count"
 
         x_values = [d["capture_count"] for d in data]  # Use capture count as x-values for horizontal bars
         y_values = [SPECIES[d["species_number"]]["alpha_code"] for d in data]
@@ -84,7 +61,7 @@ class BirdsView(TemplateView):
         fig = px.bar(
             x=x_values,  # Use capture count as x-values
             y=y_values,  # Use species code as y-values
-            title="Species Capture Count",
+            title=title,
             labels={
                 "x": "Capture Count",  # Update labels accordingly
                 "y": "Species",         # Update labels accordingly
@@ -93,7 +70,9 @@ class BirdsView(TemplateView):
             color=colors,
             orientation="h",  # Set orientation to horizontal
         )
+
         fig.update_traces(textfont_size=20, textangle=0, textposition="inside")
+
         fig.update_layout(
             title={
                 "font_size": 22,
@@ -104,14 +83,17 @@ class BirdsView(TemplateView):
             barmode="relative",
             bargap=0.2,
             height=100 + 25 * y_values_count,
+            margin=dict(l=0, r=0, t=100, b=0),
+            yaxis_title="",
         )
 
         return fig.to_html()
 
 
-    def get_chart_sex_capture_count(self, date=None):
-        if date:
-            data = CaptureRecord.objects.filter(capture_time__date=date).values("sex").annotate(capture_count=Count("sex"))
+    def get_chart_sex_capture_count(self, date_range=None):
+        if date_range:
+            start_date, end_date = date_range
+            data = CaptureRecord.objects.filter(capture_time__date__range=[start_date, end_date]).values("sex").annotate(capture_count=Count("sex"))
         else:
             data = CaptureRecord.objects.values("sex").annotate(capture_count=Count("sex"))
 
@@ -141,9 +123,11 @@ class BirdsView(TemplateView):
         )
         return fig.to_html()
 
-    def get_chart_age_capture_count(self, date=None):
-        if date:
-            data = CaptureRecord.objects.filter(capture_time__date=date).values("age_annual").annotate(capture_count=Count("age_annual"))
+    def get_chart_age_capture_count(self, date_range=None):
+        if date_range:
+            start_date, end_date = date_range
+            data = CaptureRecord.objects.filter(capture_time__date__range=[start_date, end_date]).values("age_annual").annotate(capture_count=Count("age_annual"))
+            # data = CaptureRecord.objects.filter(capture_time__date=date).values("age_annual").annotate(capture_count=Count("age_annual"))
         else:
             data = CaptureRecord.objects.values("age_annual").annotate(capture_count=Count("age_annual"))
 
