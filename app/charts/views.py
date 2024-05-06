01ee340b837a8622d5acc27840030547a3727862
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-05 19:05:33 -0700

    Make capture bar chart horizontal and top 5 groups

--- app/charts/views.py
+++ app/charts/views.py
@@ -17,37 +17,97 @@ class BirdsView(TemplateView):
         context["chart_age_capture_count"] = self.get_chart_age_capture_count(date)
         return context
 
+    # def get_chart_species_capture_count(self, date=None):
+    #     if date:
+    #         data = CaptureRecord.objects.filter(capture_time__date=date).values("species_number").annotate(capture_count=Count("species_number"))
+    #     else:
+    #         data = CaptureRecord.objects.values("species_number").annotate(capture_count=Count("species_number"))
+
+    #     x_values = [SPECIES[d["species_number"]]["alpha_code"] for d in data]
+    #     y_values = [d["capture_count"] for d in data]
+
+    #     # Get the number of x_values that will be displayed in the chart
+    #     x_values_count = len(x_values)
+
+    #     # Get the capture count of the 5th most captured species
+    #     bottom_cutoff = sorted(y_values, reverse=True)[4]
+    #     print(bottom_cutoff)
+
+    #     if not (x_values and y_values):
+    #         return ""
+        
+    #     colors = ["red" if count >= bottom_cutoff else "blue" for count in y_values]
+
+    #     fig = px.bar(
+    #         x=x_values,
+    #         y=y_values,
+    #         title="Species Capture Count",
+    #         labels={
+    #             "x": "Species",
+    #             "y": "Capture Count",
+    #         },
+    #         text_auto=True,
+    #         color=colors,
+    #     )
+    #     fig.update_traces(textfont_size=14, textangle=0, textposition="inside")
+    #     fig.update_layout(
+    #         title={
+    #             "font_size": 22,
+    #             "xanchor": "center",
+    #             "x": 0.5,
+    #         },
+    #         showlegend=False,
+    #     )
+    #     return fig.to_html()
+
     def get_chart_species_capture_count(self, date=None):
         if date:
             data = CaptureRecord.objects.filter(capture_time__date=date).values("species_number").annotate(capture_count=Count("species_number"))
         else:
             data = CaptureRecord.objects.values("species_number").annotate(capture_count=Count("species_number"))
 
-        x_values = [SPECIES[d["species_number"]]["alpha_code"] for d in data]
-        y_values = [d["capture_count"] for d in data]
+        x_values = [d["capture_count"] for d in data]  # Use capture count as x-values for horizontal bars
+        y_values = [SPECIES[d["species_number"]]["alpha_code"] for d in data]
+
+        # Get the number of y_values that will be displayed in the chart
+        y_values_count = len(y_values)
+
+        # Get the capture count of the 5th most captured species
+        top_five_lower_count = sorted(x_values, reverse=True)[4]
+        print(top_five_lower_count)
 
         if not (x_values and y_values):
             return ""
+        
+        colors = ["Top 5" if count >= top_five_lower_count else "other" for count in x_values]
 
         fig = px.bar(
-            x=x_values,
-            y=y_values,
+            x=x_values,  # Use capture count as x-values
+            y=y_values,  # Use species code as y-values
             title="Species Capture Count",
             labels={
-                "x": "Species",
-                "y": "Capture Count",
+                "x": "Capture Count",  # Update labels accordingly
+                "y": "Species",         # Update labels accordingly
             },
             text_auto=True,
+            color=colors,
+            orientation="h",  # Set orientation to horizontal
         )
-        fig.update_traces(textfont_size=14, textangle=0, textposition="inside")
+        fig.update_traces(textfont_size=20, textangle=0, textposition="inside")
         fig.update_layout(
             title={
                 "font_size": 22,
                 "xanchor": "center",
                 "x": 0.5,
             },
+            showlegend=False,
+            barmode="relative",
+            bargap=0.2,
+            height=100 + 25 * y_values_count,
         )
+
         return fig.to_html()
+
 
     def get_chart_sex_capture_count(self, date=None):
         if date:
