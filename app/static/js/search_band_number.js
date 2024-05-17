208faf7108ea24d62a7e034ff014788205c38a37
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-16 17:32:31 -0700

    Allow searching by both band number and species

--- app/static/js/search_band_number.js
+++ app/static/js/search_band_number.js
@@ -1,13 +1,12 @@
-function searchByBandNumber() {
-    // Get the search input value
-    var input = document.getElementById("searchInput");
+function searchByAny(field, element) {
+    var input = document.getElementById(field);
     var filter = input.value.toUpperCase();
     var table = document.getElementById("recordsTable");
     var tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those that don't match the search query
     for (var i = 0; i < tr.length; i++) {
-        var td = tr[i].getElementsByClassName("band-number")[0];
+        var td = tr[i].getElementsByClassName(element)[0];
         if (td) {
             var txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
