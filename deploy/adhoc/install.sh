3394d9bfce483bf1a7a6a0ff23f832ac2f8d175c
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-05-28 10:12:45 -0700

    Dropping tables and removing upload of data from csv

--- deploy/adhoc/install.sh
+++ deploy/adhoc/install.sh
@@ -10,15 +10,6 @@ python3 -m venv /srv/web/venv
 
 /srv/web/venv/bin/python3 /srv/web/app/manage.py collectstatic --noinput
 /srv/web/venv/bin/python3 /srv/web/app/manage.py migrate
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_bands /srv/web/data/Bands.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_ageannuals /srv/web/data/AgeAnnuals.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_agewrps /srv/web/data/AgeWRPs.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_groupwrps /srv/web/data/GroupWRPs.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_species /srv/web/data/TaxonBBL.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_band_allocations /srv/web/data/BandAllocations.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py setup_taxon_band_relationships
-/srv/web/venv/bin/python3 /srv/web/app/manage.py import_morphometrics /srv/web/data/Morphometrics.csv
-/srv/web/venv/bin/python3 /srv/web/app/manage.py setup_taxon_relationships
 
 cp /srv/web/deploy/adhoc/django.service /etc/systemd/system/django.service
 cp /srv/web/deploy/adhoc/django.timer /etc/systemd/system/django.timer
