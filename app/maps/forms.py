58347f74720928b85dc27844234696afaf40bdf0
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-07-02 12:27:56 -0700

    Updated band number handling and validation logic
    - Modified `forms.py`:
      - Made `band_number` field optional by setting `required=False`.
      - Added `clean_band_number` method to return `None` for unbanded birds.
      - Updated `validate_unbanded_has_no_band_number` function to check for non-empty band numbers.
      - Included `clean_note` method to replace new lines with end of sentence structure.
    
    - Enhanced JavaScript logic:
      - Combined and streamlined the logic from `capture_code.js` and `display_band.js` into `handle_band.js`.
      - Updated the display of the band number format to show underscores for missing digits.
      - Disabled band number input for unbanded birds and reset formatted display.
      - Ensured the formatted display updates dynamically as users input band numbers and on form submission.
    
    - Cleaned up and removed commented-out legacy JavaScript code.
    
    - Other minor improvements and bug fixes for better form handling and validation.

--- app/maps/forms.py
+++ app/maps/forms.py
@@ -21,11 +21,11 @@ class CaptureRecordForm(forms.ModelForm):
     # Define each part of the band_number with type "number"
     band_number = forms.CharField(
         max_length=9,
+        required=False,
         widget=forms.TextInput(
             attrs={
                 "inputmode": "numeric",
                 "class": "band-input",
-                "placeholder": "____-_____",
             },
         ),
         label="Band Number",
@@ -251,6 +251,15 @@ class CaptureRecordForm(forms.ModelForm):
         if bander_initials:
             bander_initials = bander_initials.upper()
         return bander_initials
+    
+    def clean_note(self):
+        # Remove all new lines and replace with end of sentence structure
+        return self.cleaned_data.get("note").replace("\r\n", ". ").replace("\n", ". ")
+    
+    def clean_band_number(self):
+        if self.cleaned_data.get("capture_code") == "U":
+            return None
+        return self.cleaned_data.get("band_number")
 
     def _clean_capture_time(self):
         year = int(self.cleaned_data.get("capture_year_day").year)
